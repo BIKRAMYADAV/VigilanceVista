f92e1b15ce3eccd21345f2b6b7048b56244af7ba
Bikram Yadav <124283481+BIKRAMYADAV@users.noreply.github.com>
2024-02-11 17:58:50 +0530

    enabled authentication

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose')
 const app = express()
 const path = require('path')
 const PORT = 3000
-const URI = process.env.MONGO_URI
+const bcrypt = require('bcryptjs')
+const URI = "mongodb+srv://admin:admin@blog-website.utuektf.mongodb.net/?retryWrites=true&w=majority"
 const usermodel = require('./models/community.js')
+const userAuthModel = require('./models/user.js')
 const func = async () => {
   await mongoose.connect(URI);
 }
@@ -22,19 +24,50 @@ func().then(() => {
     console.log(err);
 })
 
+//home
+app.get('/home', (req, res) => {
+    res.render('home.ejs')
+})
 
+//register
 app.get('/', (req,res) => {
-    res.render('community.ejs');
+    res.render('register.ejs');
 });
 
 app.post('/',async (req, res) => {
-    const {name, post} = req.body;
-    const user = new usermodel({
+    const {name, email, password} = req.body;
+    let user = await userAuthModel.findOne({email});
+    if(user){
+        return res.redirect('/');
+    }
+
+    const hashedPassword = await bcrypt.hash(password,12);
+
+     user = new userAuthModel({
         name
-        , post
+        , email,
+        password : hashedPassword
     });
 
     await user.save();
+    res.redirect('/login');
+})
+
+//login
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+app.post('/login', async (req, res) => {
+    const {email, password} = req.body;
+    const user = await userAuthModel.findOne({email})
+    if(!user){
+        return res.redirect('/');
+    }
+    const isMatched = await bcrypt.compare(password, user.password);
+    if(!isMatched){
+        return res.redirect('/login');
+    }
+    res.redirect('/home');
 })
 
 
