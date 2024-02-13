9b6feb093d47af97adee4d60de9fbd27f5261118
Bikram Yadav <124283481+BIKRAMYADAV@users.noreply.github.com>
2024-02-13 16:21:25 +0530

    Update community.js

--- backend/models/community.js
+++ backend/models/community.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const PostSchema = new Schema({
     name : {
-      type : String,
-      required : true
+        type : String,
+        required : true
     },
-    post : {
-       type : String,
-       required : true
-    }
+    content : {
+        type : String,
+        required : true
+    },
 })
 
-module.exports = mongoose.model("user",userSchema);
+module.exports = mongoose.model('postModel',PostSchema);
