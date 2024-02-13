db4eb478ff2e9482efb26cb15d56ff493bc4dbc8
Bikram Yadav <124283481+BIKRAMYADAV@users.noreply.github.com>
2024-02-13 16:20:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,23 @@ const path = require('path')
 const PORT = 3000
 const bcrypt = require('bcryptjs')
 const URI = "mongodb+srv://admin:admin@blog-website.utuektf.mongodb.net/?retryWrites=true&w=majority"
-const usermodel = require('./models/community.js')
-const userAuthModel = require('./models/user.js')
-const func = async () => {
-  await mongoose.connect(URI);
-}
+const postModel = require('./models/Post.js')
+const userAuthModel = require('./models/User.js')
 
+
+
+
+
+//middleware
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended : true}))
+
+
+
+//mongodb connection setup
+const func = async () => {
+    await mongoose.connect(URI);
+  }
 
 func().then(() => {
     console.log('connected to the database');
@@ -23,6 +32,9 @@ func().then(() => {
 }).catch((err) => {
     console.log(err);
 })
+
+
+//ROUTES : 
 
 //home
 app.get('/home', (req, res) => {
@@ -53,6 +65,7 @@ app.post('/',async (req, res) => {
     res.redirect('/login');
 })
 
+
 //login
 app.get('/login', (req, res) => {
     res.render('login');
@@ -67,6 +80,21 @@ app.post('/login', async (req, res) => {
     if(!isMatched){
         return res.redirect('/login');
     }
+    res.redirect('/home');
+})
+
+
+//community
+app.get('/community', (req, res) => {
+    res.render('community.ejs');
+})
+
+app.post('/community', async (req, res) => {
+    const {name, content} = req.body;
+    const post = new postModel({
+        name, content
+    })
+    await post.save();
     res.redirect('/home');
 })
 
