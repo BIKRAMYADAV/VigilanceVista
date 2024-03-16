150bc6ea8582438ab169ac6176d9cf56cc0f5942
Bikram Yadav <124283481+BIKRAMYADAV@users.noreply.github.com>
2024-03-16 23:15:28 +0530

    Update Zone.jsx

--- Front end/src/pages/Zone.jsx
+++ Front end/src/pages/Zone.jsx
@@ -8,7 +8,7 @@ function Zone() {
         <div className="w-full mx-auto">
             <div className="aspect-w-16 aspect-h-9 rounded-lg overflow-hidden" style={{height: "calc(100vh - 100px)"}}>
                 <iframe 
-                    src="https://sdd1leo.github.io/map/" 
+                    src="https://map-ml-cybersec.streamlit.app/" 
                     frameBorder="0"  
                     className="w-full h-full"
                 ></iframe>
