9783aa2472efe6eb97d5e6876b0c171c30d0ad0c
Bikram Yadav <124283481+BIKRAMYADAV@users.noreply.github.com>
2024-03-16 22:09:29 +0530

    removed middleware

--- back-end/index.js
+++ back-end/index.js
@@ -15,12 +15,6 @@ app.use(cors(
     }
 ))
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow specified HTTP methods
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specified headers
-    next();
-  });
 
 const func = async () => {
 await mongoose.connect('mongodb+srv://admin:admin@blog-website.utuektf.mongodb.net/?retryWrites=true&w=majority')
